c4dd71cda56084ae6e666f0b8059f4de545b6001
SWT2006 <SarabiaDania738@gmail.com>
2025-10-31 10:34:17 +0800

    Force push to overwrite remote repository

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,11 +1,4 @@
 #include "Utils.h"
 #include<iostream>
- class die{
- public:
-    double width,height;
-    bool issoft;
-    std::string name;
-    die(double w, double h) : width(w), height(h), issoft(false) {}
- };
 
- 
+
